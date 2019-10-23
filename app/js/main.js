b94c36d607233fe36e6ffa6831d8a5f691eb531f
SergiyDanchuk <sergey.danchuk.p@gmail.com>
2019-10-23 21:30:41 +0300

    added works, news and quote section

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
 
     $('.header__slider').slick({
         dots: true,
@@ -6,6 +6,14 @@ $(function(){
         speed: 1000
     });
 
+    $('a[href^="#"]').click(function () {
+        var el = $(this).attr('href');
+        $('body').animate({
+            scrollTop: $(el).offset().top
+        }, 2000);
+        return false;
+    });
 
 
+    var mixer = mixitup('.works__inner-box');
 });
